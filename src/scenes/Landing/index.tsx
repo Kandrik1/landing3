31eb97d773455b9b9a0b1b8f0246db597a31d523
Dante Hatake <willpar37@gmail.com>
2023-03-14 17:32:05 +0300

    update architecture

---
+++ src/scenes/Landing/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.module.sass";
+
+export default function Index() {
+
+  return (
+    <div>
+      <SubscribeSection/>
+      <div>
+        <p>
+          I subscribed to this newsletter because I heard it's really creative. The last thing I want is for my inbox to
+          be cluttered with the same old stuff. Every day when I open my email, I find something new and exciting in
+          Noni's emails. You name it, they mention it in their newsletter!
+        </p>
+      </div>
+    </div>
+  )
+}
